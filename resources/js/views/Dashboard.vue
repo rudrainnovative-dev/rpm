1d4e3e95f8260bd111e5b2260c2161f7a3f0b171
Parambir Singh <parambir@rudrainnovatives.com>
2022-09-07 19:07:56 +0530

    small change in dashboard

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -3,7 +3,7 @@
     <div class="toolbar" id="kt_toolbar">
       <div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">
         <div data-kt-place="true" data-kt-place-mode="prepend" data-kt-place-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" class="page-title d-flex align-items-center me-3 flex-wrap mb-5 mb-lg-0 lh-1">
-          <h1 class="d-flex align-items-center text-dark fw-bolder my-1 fs-3">Dashboard </h1>
+          <h1 class="d-flex align-items-center text-dark fw-bolder my-1 fs-3">Dashboard</h1>
         </div>
       </div>
     </div>
